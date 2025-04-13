285cc4c546adc90b63dc163340e5fce6276a0af3
dario <dasvh@protonmail.com>
2025-04-13 17:01:26 +0200

    add bashls language server

--- init.lua
+++ init.lua
@@ -643,6 +643,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
+        bashls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
