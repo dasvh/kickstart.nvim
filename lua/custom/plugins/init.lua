b4927fa13ef6c17ff4a372ea6ad9b50470e73c76
dario <dasvh@protonmail.com>
2026-04-18 10:15:17 +0200

    feat: add lazygit.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -15,4 +15,19 @@ return {
       },
     }
   end,
+
+  {
+    'kdheepak/lazygit.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    keys = {
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'Open LazyGit' },
+    },
+  },
 }
